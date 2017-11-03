55c09a6748fd46c1f7d2fd9a2edcbabd68fab927
Nour-Audi <nourvaudi@gmail.com>
2017-11-03 16:13:16 -0400

    make file .R file

---
+++ final_project/makefile.R
@@ -0,0 +1,15 @@
+## clean all output from previous runs of scripts
+unlink("final_project/data", recursive = TRUE) 
+unlink("final_project/graph", recursive = TRUE) 
+
+# now re-create the results directory
+dir.create(file.path("final_project/data"), showWarnings = FALSE)
+dir.create(file.path("final_project/graph"), showWarnings = FALSE)
+
+
+## run all scripts
+source("final_project/00_ setting up R .R") # setting up R enviroment 
+source("final_project/01_ web_scraping.R")  # importing and cleaning data
+source("final_project/03_ getting and cleaning characters list.R") # webscraping and cleaning of characters names 
+source("final_project/04_ Analysis.R")  # analysis and questions answering 
+rmarkdown::render("final_project/paper.Rmd", output_format = "pdf_document")
