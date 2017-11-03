040a1fe2c4051a015d2f0f1906466d1b8b0e1c1e
Nour-Audi <nourvaudi@gmail.com>
2017-11-03 02:11:27 -0400

    repaire

--- Nour/01_ setting up R .R
+++ Nour/01_ setting up R .R
@@ -1,6 +1,6 @@
 # check that all required packages are installed 
 source("Nour/check_packages.R")
-check.packages(c("ggplot2", "dplyr", "stringi", "harrypotter", "tidytext", "rebus"))
+check.packages(c("ggplot2", "dplyr", "stringi", "stringr", "harrypotter", "tidytext", "rebus"))
 # making sure harry potter version is uptodate
 if (packageVersion("devtools") < 1.6) {
   install.packages("devtools")}
