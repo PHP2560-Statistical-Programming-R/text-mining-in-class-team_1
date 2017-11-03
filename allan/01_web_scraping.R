ece1a6fdf34e68cb03b03e83631f19ef9ebaaee2
kimaina <kimkenal@gmail.com>
2017-11-03 16:20:08 -0400

    fixed book metadata

--- allan/01_web_scraping.R
+++ allan/01_web_scraping.R
@@ -1,9 +1,6 @@
 
 
 get_harry_potter_characters <- function(){
-  # Fetch Book metadata from theGuardian.com e.g publisher, ranking, sales, author e.t.c 
-  bookMetadata <- read.csv("https://docs.google.com/spreadsheets/d/1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA/export?format=csv&id=1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA&gid=0")
-  
   #web scrape main characters of Harry Potter Series from
   # download harry potter wikipedia page
   hp_wikipedia_site = readLines('https://en.wikipedia.org/wiki/List_of_Harry_Potter_characters')
@@ -23,4 +20,13 @@ get_harry_potter_characters <- function(){
   
 }
 
+get_book_metadata(){
+  # Fetch Book metadata from theGuardian.com e.g publisher, ranking, sales, author e.t.c 
+  bookMetadata <- read.csv("https://docs.google.com/spreadsheets/d/1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA/export?format=csv&id=1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA&gid=0")
+  
+  save(bookMetadata, file="allan/data/book_metadata.rda")
+
+}
+
 get_harry_potter_characters()
+get_book_metadata()
