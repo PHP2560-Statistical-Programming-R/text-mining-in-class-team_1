9e6aa936933e1d580975f636d5cceaff9cfbb5e9
Nour-Audi <nourvaudi@gmail.com>
2017-11-03 15:16:37 -0400

    adding dependincies= TRUE

--- final_project/check_packages.R
+++ final_project/check_packages.R
@@ -3,7 +3,7 @@ check.packages = function(names)
   for(name in names)
   {
     if (!(name %in% installed.packages()))
-      install.packages(name, repos="http://cran.us.r-project.org")
+      install.packages(name, repos="http://cran.us.r-project.org",dependencies=TRUE)
     
     library(name, character.only=TRUE)
   }
