37266d8e5a8a4c01c9c00190818ee91c1480c54d
kimaina <kimkenal@gmail.com>
2017-11-06 17:44:56 -0500

    clean up and merged the 2 versions of half blood prince

--- final_project/01_web_scraping.R
+++ final_project/01_web_scraping.R
@@ -1,9 +1,6 @@
 
 
 get_harry_potter_characters <- function(){
-  # Fetch Book metadata from theGuardian.com e.g publisher, ranking, sales, author e.t.c 
-  bookMetadata <- read.csv("https://docs.google.com/spreadsheets/d/1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA/export?format=csv&id=1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA&gid=0")
-  
   #web scrape main characters of Harry Potter Series from
   # download harry potter wikipedia page
   hp_wikipedia_site = readLines('https://en.wikipedia.org/wiki/List_of_Harry_Potter_characters')
@@ -18,9 +15,29 @@ get_harry_potter_characters <- function(){
     filter(!FirstName%in%custom_stop_words) %>% # clean data by first name
     filter(!LastName%in%custom_stop_words) %>% # clean data by last name 
     unique() 
-  save(bookMetadata, file = "final_project/data/bookMetadata.rda")
   save(harrypotter_characters, file="final_project/data/harrypotter_characters.rda")
   
 }
 
+
+get_book_metadata<-function(){
+  # Fetch Book metadata from theGuardian.com e.g publisher, ranking, sales, author e.t.c 
+  bookMetadata <- read.csv("https://docs.google.com/spreadsheets/d/1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA/export?format=csv&id=1dhxblR1Vl7PbVP_mNhwEa3_lfUWiF__xSODLq1W83CA&gid=0")%>%
+    mutate(Volume.Sales = as.numeric(gsub(",", "", Volume.Sales)))
+  
+  # Harry Potter and the Half-blood Prince has 2 versions i.e Children's Edition and main Edition
+  # we need to sum up the sales for the 2 versions of "Harry Potter and the Half-blood Prince"
+  merged <- bookMetadata %>%
+    filter(grepl('Harry Potter and the Half-blood Prince', Title))%>%
+    summarize(sum = sum(Volume.Sales))
+  
+  bookMetadata <- bookMetadata %>%
+    mutate(Volume.Sales = ifelse(Title == "Harry Potter and the Half-blood Prince",
+                                 merged$sum, Volume.Sales)) 
+  
+  save(bookMetadata, file = "final_project/data/bookMetadata.rda")
+  
+}
+
 get_harry_potter_characters()
+get_book_metadata()
