cb7cb5e5d9c65adeaaacee6931b0bc9c90d95199
Wei Wang <30543922+Wayyyy@users.noreply.github.com>
2017-11-07 19:55:41 -0500

    12 Questions

---
+++ text_mining_Wei_Wang/02_shape_data.R
@@ -0,0 +1,18 @@
+
+hp_books <- c("Harry Potter and the Philosopher's Stone", 
+              "Harry Potter and the Chamber of Secrets", 
+              "Harry Potter and the Prisoner of Azkaban",
+              "Harry Potter and the Goblet of Fire", 
+              "Harry Potter and the Order of the Phoenix", 
+              "Harry Potter and the Half-blood Prince",
+              "Harry Potter and the Deathly Hallows"
+)
+
+hp_list <- list(harrypotter::philosophers_stone, 
+                harrypotter::chamber_of_secrets, 
+                harrypotter::prisoner_of_azkaban,
+                harrypotter::goblet_of_fire, 
+                harrypotter::order_of_the_phoenix, 
+                harrypotter::half_blood_prince,
+                harrypotter::deathly_hallows
+)
